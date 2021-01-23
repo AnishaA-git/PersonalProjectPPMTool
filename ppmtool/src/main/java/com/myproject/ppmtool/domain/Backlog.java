9504a5a152f62f0c7b78f16e8cec870e2d63102c
AnishaA-git <anisha.agarwal1993@gmail.com>
2021-01-22 23:22:38 -0800

    code for project tasks

--- ppmtool/src/main/java/com/myproject/ppmtool/domain/Backlog.java
+++ ppmtool/src/main/java/com/myproject/ppmtool/domain/Backlog.java
@@ -1,11 +1,16 @@
 package com.myproject.ppmtool.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -25,11 +30,11 @@ public class Backlog {
     @JsonIgnore
     private Project project;
     
-    
     //OneToMany projecttasks
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.REFRESH, mappedBy = "backlog", orphanRemoval = true)
+    private List<ProjectTask> projectTasks = new ArrayList<>();
 
-
-    public Backlog() {
+	public Backlog() {
     }
 
     public Long getId() {
@@ -63,4 +68,13 @@ public class Backlog {
 	public void setProject(Project project) {
 		this.project = project;
 	}
+	
+	public List<ProjectTask> getProjectTasks() {
+		return projectTasks;
+	}
+
+	public void setProjectTasks(List<ProjectTask> projectTasks) {
+		this.projectTasks = projectTasks;
+	}
+
 }
